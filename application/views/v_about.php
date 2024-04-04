c248cf34dbf4f9fb268a5de141bdc69ca919a521
aldiansyahramli <btldream7@gmail.com>
2024-04-04 09:21:56 +0700

    Test 1

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section> 
+<h1><?php echo $judul ?></h1> 
+<h4>Nama</h4> 
+    <ul type="disc"> 
+        <li>Nama Depan : Aldiansyah</li> 
+        <li>Nama Belakang : Ramli</li> 
+    </ul> 
+    <br> 
+    <h4>Alamat</h4> 
+    <ul type="none"> 
+        <li> Jalan Manggis IV Bukit Waringin </li> 
+    </ul> 
+     
+    <h4>Tempat Lahir</h4> 
+    <ul type="none"> 
+        <li>Bogor</li> 
+    </ul> 
+ 
+    <h4>Olah Raga Favorit</h4> 
+    <ul type="square"> 
+        <li>Sepak Bola</li> 
+        <li>Gulat</li> 
+    </ul> 
+</section>
---
+++ application/views/v_footer.php
@@ -0,0 +1,6 @@
+<footer> 
+            <a href="https://www.youtube.com/">RentalBuku</a> 
+        </footer> 
+    </div> 
+</body> 
+</html> 
